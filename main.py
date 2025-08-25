2269e574ea7655072a124dd0cd47f76cd2c03d1f
itzboss799 <itzboss799@gmail.com>
2025-08-25 22:54:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"R44JV33R B0SS S3RV3R")
+        self.wfile.write(b"RAAJVEER BOSS OFFLINE POST SERVER")
 
 def execute_server():
     PORT = 4000
